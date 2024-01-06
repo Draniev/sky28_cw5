0a2e9a241f627dfe5c7619988ba6f8c9123846e6
Sergei Popov <sergei.v.popov@icloud.com>
2024-01-06 22:36:23 +0700

    ref: немного перебрал ЮИ для новой задачи

--- user_interface.py
+++ user_interface.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 from rich import print
+from saveload import JSONSaveLoadVacancies
 
 from hh_vacancyapi import HHVacancyAPI
 from load_env import SJ_API_KEY
-from saveload import JSONSaveLoadVacancies
 from sj_vacancyapi import SJVacancyAPI
 from vacancy import Vacancy
 from vacancyapi import VacancyApi
@@ -12,10 +12,10 @@ from vacancyapi import VacancyApi
 
 class UIState(Enum):
     IDLE = 'Ожидаем начала работы'
-    LOAD_FILE = 'Ожидаем загрузку вакансий из файла'
-    API_SEARCH0 = 'Выбираем сайт для поиска вакансий по АПИ'
+    LOAD = 'Работа с БД'
+    API_SEARCH0 = 'Добавить работодателя в БД'
     API_SEARCH1 = 'Загружаем критерии поиска вакансий'
-    VACANCY = 'Режим работы с загруженными вакансиями'
+    # VACANCY = 'Режим работы с загруженными вакансиями'
     EXIT = 'Выключаемся'
 
 
@@ -24,11 +24,12 @@ class UserInterface:
     def __init__(self):
         self.__state = UIState.IDLE
         self.hh_api = HHVacancyAPI()
-        self.sj_api = SJVacancyAPI(SJ_API_KEY)
+        # self.sj_api = SJVacancyAPI(SJ_API_KEY)
         self.apies: list[VacancyApi] = []
-        self.vacancies: list[Vacancy] = []
+        self.vacancies: list[Vacancy] = []  # Локальная выборка для работы
         self.print_qty: int = 10  # количество объектов на печать
-        self.loader = JSONSaveLoadVacancies()
+
+        self.apies.append(self.hh_api)
 
     def start(self):
         is_exit = False
@@ -43,14 +44,14 @@ class UserInterface:
         match self.__state:
             case UIState.IDLE:
                 is_exit = self.__idle_input()
-            case UIState.LOAD_FILE:
-                is_exit = self.__load_file()
+            case UIState.LOAD:
+                is_exit = self.__working_with_vacancy()
             case UIState.API_SEARCH0:
                 is_exit = self.__search0_input()
             case UIState.API_SEARCH1:
-                is_exit = self.__find_vacancy_ui()
-            case UIState.VACANCY:
-                is_exit = self.__working_with_vacancy()
+                is_exit = self.__add_employee_do_db()
+            # case UIState.VACANCY:
+            #     is_exit = self.__working_with_vacancy()
             case _:
                 is_exit = True  # В любой не понятной ситуации - досвидули!
 
@@ -60,15 +61,15 @@ class UserInterface:
 
     def __idle_input(self) -> bool:
 
-        print('(1) Поработать с локальными файлами '
-              '\n(2) найти новые вакансии, '
+        print('(1) Поработать с БАЗОЙ или '
+              '\n(2) добавить новые компании, '
               '\n(9, exit) выйти?')
 
         user_input = input('Выберите действие: 1, 2 или 9 >> ')
 
         match user_input:
             case '1':
-                self.__state = UIState.LOAD_FILE
+                self.__state = UIState.LOAD
             case '2':
                 self.__state = UIState.API_SEARCH0
             case '9' | 'exit':
@@ -76,113 +77,29 @@ class UserInterface:
 
         return False
 
-    def __load_file(self) -> bool:
-
-        filename = input('(8, back) назад или выйти (9, exit)\n'
-                         'Имя файла для загрузки. "Enter" - по умолчанию. >> ')
-
-        match filename:
-            case '8' | 'back':  # Возврат на предыдуший шаг
-                self.__state = UIState.IDLE
-                return False
-            case '9' | 'exit':
-                return True  # Завершение и выход
-
-        try:
-            self.vacancies = []
-            if len(filename) == 0:
-                self.vacancies = self.loader.loadfile()
-            else:
-                self.vacancies = self.loader.loadfile(filename)
-
-            print(f'Из файла загружено {len(self.vacancies)} вакансий')
-            self.__state = UIState.VACANCY
-
-        except FileNotFoundError:
-            print('Файл повреждён или не найден. Попробуем еще раз?')
-
-        return False  # Продолжаем работу в цикле
-
-    def __safe_to_file(self, vacancies: list[Vacancy]) -> bool:
-
-        filename = input('(8, back) назад\n'
-                         'Введите имя файла. "Enter" - по умолчанию. >> ')
-
-        match filename:
-            case '8' | 'back':  # Возврат на предыдуший шаг
-                return False
-
-        if len(filename) == 0:
-            self.loader.savefile(vacancies)
-        else:
-            self.loader.savefile(vacancies, f'{filename}.json')
-
-        print(f'Записано в файл {len(self.vacancies)} вакансий')
-
-        return False  # Продолжаем работу в цикле
-
-    def __search0_input(self) -> bool:
-
-        print('Поискать \n(1) на HeadHunter '
-              '\n(2) на SuperJob '
-              '\n(3) на всех доступных платформах '
-              '\n(8, back) назад или выйти (9, exit)?')
-
-        user_input = input('Выберите действие: 0, 1, 2 или 8, 9 >> ')
-
-        match user_input:
-            case '1':  # Поиск на ХХ
-                self.apies = [self.hh_api]
-            case '2':  # Поиск на SJ
-                self.apies = [self.sj_api]
-            case '3':  # Поиск на обоих платформах
-                self.apies = [self.hh_api, self.sj_api]
-            case '8' | 'back':  # Возврат на предыдуший шаг
-                self.__state = UIState.IDLE
-                return False
-            case '9' | 'exit':
-                return True  # Завершение и выход
-
-        self.__state = UIState.API_SEARCH1
-        return False
-
-    def __find_vacancy_ui(self) -> bool:
+    def __add_employee_do_db(self) -> bool:
 
         keyword = input('(8, back) назад или выйти (9, exit)\n'
-                        'Ключевое слово для поиска: ')
+                        'Название работодателя для парсинга')
 
         match keyword:
             case '8' | 'back':  # Возврат на предыдуший шаг
-                self.__state = UIState.API_SEARCH0
+                self.__state = UIState.IDLE
                 return False
             case '9' | 'exit':
                 return True  # Завершение и выход
-
-        salary = input('(8, back) назад или выйти (9, exit)\n'
-                       'Примерная зарплата в RUR (0 если не важно): ')
-
-        match salary:
-            case '0':
-                salary = None
-            case '8' | 'back':  # Возврат на предыдуший шаг
-                self.__state = UIState.API_SEARCH0
-                return False
-            case '9' | 'exit':
-                return True  # Завершение и выход
-            case _:
-                salary = int(salary) if salary.isnumeric() else None
 
         self.vacancies = []
         for api in self.apies:
             print(f'Начинаем парсинг {api} '
                   f'для вакансий по ключу "{keyword.upper()}"')
-            vacancies = api.get_vacancy(keyword, salary)
+            # vacancies = api.get_vacancy(keyword, salary)
             print(f'Найдено {len(vacancies)} с помощью {api}')
             self.vacancies.extend(vacancies)
 
         print(f'Используя {len(self.apies)} апи '
               f'получено {len(self.vacancies)} вакансий')
-        self.__state = UIState.VACANCY
+        self.__state = UIState.IDLE
         return False  # Продолжаем работу в цикле
 
     def __working_with_vacancy(self) -> bool:
@@ -215,8 +132,6 @@ class UserInterface:
                 self.__filter_city()
             case '5':
                 self.__filter_word()
-            case 's':
-                self.__safe_to_file(self.vacancies)
             case '9' | 'exit':
                 return True  # Завершение и выход
 
